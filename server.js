12e7d8d58eefef3cabab3def267e68d0c0d60227
manyasiingh <manya230702@gmail.com>
2025-12-03 10:29:32 +0530

    modified:   client/build/asset-manifest.json

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require("path");
 const notificationRouter = require("./routes/notificationRouter");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
